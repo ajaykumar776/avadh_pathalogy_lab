d5f13698eeed7a6757a6a7375ddb1caaac4400a1
ajaykumar776 <ajaykrdtg5@gmail.com>
2021-03-25 12:05:56 +0530

    new file

--- basic/views/billitem/bill.php
+++ basic/views/billitem/bill.php
@@ -21,67 +21,59 @@ use yii\db\Connection;
 
 
 ?>
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-    <style>
-        @media print {
-            body * {
-                visibility: hidden;
-            }
-            .print,.print *{
-                visibility: visible;
 
-            }
-
-            
-
+<style>
+    @media print {
+        body * {
+            visibility: hidden;
         }
-    </style>
-</head>
+        .print,.print *{
+            visibility: visible;
+        }   
+
+    }
+</style>
 <body><br><br><br><br>
 <div class="container ">
-		<div class="row">
-			<div class="col-md-12 print">
-				<table class="table table-bordered table-hover">
-					<thead class="thead-dark">
-						<tr>
-							<th>ITEMS</th>
-							<th>UNIT-RATE</th>
-							<th>QUANTITY</th>
-							<th>AMOUNT</th>
-						</tr>
-					</thead>
-					<tbody>
-							<?php foreach($billitems as $billitem){?>
-                               <?php $data= $billitem->category_id;?>
+    <div class="row">
+        <div class="col-md-12 print">
+            <table class="table table-bordered table-hover">
+                <thead class="thead-dark">
+                    <tr>
+                        <th>ITEMS</th>
+                        <th>UNIT-RATE</th>
+                        <th>QUANTITY</th>
+                        <th>AMOUNT</th>
+                    </tr>
+                </thead>
+                <tbody>
+                        <?php foreach($billitems as $billitem){?>
 
-                                <?php $subcategory =  Scategory::find()->where(['category_id'=> $billitem->category_id])->where(['amount'=> $billitem->unit_rate])->one();?>
-                                <?php $total = HospitalBill::find()->where(['id'=> $billitem->hospital_bill_id])->one();?>
-                                <tr>
-                                    <td><?php  echo $subcategory->name;?></td>
-                                    <td><?php  echo $billitem->unit_rate; ?></td>
-                                    <td><?php  echo $billitem->quantity; ?></td>
-                                    <td><?php  echo $billitem->amount; ?></td>
-                                    
-                                </tr>
-                            <?php }?>
-
-                        
-									
-					</tbody>
-						
-				</table> <h2>Total Amount :<span class="text-align:left"><?php  echo $total->total_charges;?></span><h2>
-                
-			</div>
+                            <?php $subcategory =  Scategory::find()->where(['category_id'=> $billitem->category_id])->where(['amount'=> $billitem->unit_rate])->one();?>
             
-		</div>
-        <button type="button" id="btn" onclick="window.print()" class="btn btn-outline-info ">PRINT </button>
+                            <tr>
+                                <td><?php  echo $subcategory->name;?></td>
+                                <td><?php  echo $billitem->unit_rate; ?></td>
+                                <td><?php  echo $billitem->quantity; ?></td>
+                                <td><?php  echo $billitem->amount; ?></td>
+                                
+                            </tr>
+                        <?php }?>
+
+                    
+                                
+                </tbody>
+                    
+            </table> 
+            <?php $total = HospitalBill::find()->where(['id'=> $billitem->hospital_bill_id])->one();?>
+            <h2>Total Amount :<span class="text-align:left"><?php  echo $total->total_charges;?></span><h2>
+            <h4>Discount :<span class="text-align:left"><?php  echo $total->discount;?></span><h4>
+            <h4>Remaining Amount :<span class="text-align:left"><?php  echo $total->remaining_amount;?></span><h4>
+            
+        </div>
+        
     </div>
-</body>
-</html>
+    <button type="button" id="btn" onclick="window.print()" class="btn btn-outline-info ">PRINT </button>
+</div>
    
    
